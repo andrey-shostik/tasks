73b8388a533b8dd9ad8af17aa6d96953ce424bec
andrey9828 <aaa73731919@gmail.com>
2016-05-13 01:14:50 +0300

    Add test to models use shoulda matcher

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Profile do
   let(:user) { FactoryGirl.create(:user) }
-  let(:profile) { FactoryGirl.build(:profile, user: user) }
+  let(:item) { FactoryGirl.build(:item, user: user) }
 
   describe 'validations' do
-    it { validate_presence_of(:first_name) }
-    it { expect(profile).to be_valid }
+    it { validate_presence_of(:title) }
+    it { validate_presence_of(:description) }
+    it { expect(item).to be_valid }
   end
 
   describe 'associations' do
