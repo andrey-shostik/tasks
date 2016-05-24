c7f877c111339e255bd728da04c93d6da4f9cde7
andrey9828 <aaa73731919@gmail.com>
2016-05-25 01:23:11 +0300

    Add admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523201954) do
+ActiveRecord::Schema.define(version: 20160524195443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20160523201954) do
     t.string   "token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
   add_foreign_key "accounts", "users"
