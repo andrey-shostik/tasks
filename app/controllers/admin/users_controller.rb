69adf3d252cdf262b812b922b1049f762ca82639
andrey9828 <aaa73731919@gmail.com>
2016-05-25 17:49:41 +0300

    Add destroy users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,17 @@
 class Admin::UsersController < AdminsController
+  before_action :set_user, only: [:destroy]
+
   def index
     @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
