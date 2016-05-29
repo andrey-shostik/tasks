6ad517508bc65c3f23bc75da3b88f6b6b212235a
andrey9828 <aaa73731919@gmail.com>
2016-05-29 13:56:04 +0300

    hot fix admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,3 +1,14 @@
 class AdminsController < ActionController::Base
   layout 'admin/application'
+  before_action :require_admin
+
+  def current_user
+    @current_user ||= User.find_by(token: session[:current_user_id])
+  end
+
+  private
+
+  def require_admin
+    redirect_to root_path unless current_user.admin == true
+  end
 end
